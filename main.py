bfa493169d3338967a559281e5aeedecfc2e398f
chefirchy <42928834+chefirchy@users.noreply.github.com>
2020-12-14 13:12:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+# pip install pymorphy2
 import pymorphy2
 import string
+
 morph = pymorphy2.MorphAnalyzer()
 
-word = "стали"
-p = morph.parse(word)[0]
-print(p.normal_form)
+word = "женская"
+def tags(word):
+    p = morph.parse(word)[0]
+    print(p.normal_form)
+    print(p.tag.gender)
+    print(p.tag)
+    return morph.parse(word)
+print(tags(word))
