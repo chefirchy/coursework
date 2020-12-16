d93f1d9b3352d8f5c7f3a0a23d4815d49574b2fe
chefirchy <42928834+chefirchy@users.noreply.github.com>
2020-12-16 19:09:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,18 @@ import string
 
 morph = pymorphy2.MorphAnalyzer()
 
-word = "женская"
+word = input()
+
 def tags(word):
     p = morph.parse(word)[0]
     print(p.normal_form)
     print(p.tag.gender)
     print(p.tag)
-    return morph.parse(word)
+    #return morph.parse(word)
 print(tags(word))
+
+s = input()
+
+lst = s.replace('.', '').split()
+for word in lst:
+    print(tags(word))
