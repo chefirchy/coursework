8fd09acc06577194915c0f081893f3d88c69c330
chefirchy <42928834+chefirchy@users.noreply.github.com>
2021-04-09 10:23:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 # pip install pymorphy2
 import pymorphy2
+import PyQt5
 import string
+import json
+import re
+
 
 morph = pymorphy2.MorphAnalyzer()
 
 word = input()
-
+word = re.split('[^a-zа-яё]+', word, flags=re.IGNORECASE)
+print(word)
 def tags(word):
     p = morph.parse(word)[0]
     print(p.normal_form)
@@ -16,6 +21,6 @@ print(tags(word))
 
 s = input()
 
-lst = s.replace('.', '').split()
+lst = s.replace('.', ' ').split()
 for word in lst:
     print(tags(word))
